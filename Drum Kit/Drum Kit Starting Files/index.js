e1c0f557aa45d32512f14c11ab519a8801fcf713
Rahul <rahul2001ramesh@gmail.com>
2022-04-03 22:44:17 +0530

    finalcommit

--- Drum Kit/Drum Kit Starting Files/index.js
+++
@@ -1,7 +0,0 @@
-
-var numberofdrumbuttons = document.querySelector(".drum").length; 
-for (var i = 0; i < numberofdrumbuttons;i++){
-    document.querySelector(".drum")[i].addEventListener("click", function () {
-    
-    });
-}
