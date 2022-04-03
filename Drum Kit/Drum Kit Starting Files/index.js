900934fe09fee25c9f5bbc9a6a882d5eb53751b5
Rahul <rahul2001ramesh@gamil.com>
2022-04-03 17:45:30 +0530

    first commit

---
+++ Drum Kit/Drum Kit Starting Files/index.js
@@ -0,0 +1,8 @@
+
+var numberofdrumbuttons = document.querySelector(".drum").length; 
+for (var i = 0; i < numberofdrumbuttons;i++){
+    document.querySelector(".drum")[i].addEventListener("click", function () {
+    
+    });
+
+}
